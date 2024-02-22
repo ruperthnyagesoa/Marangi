f445231abf8f920a12d42ebe42ddf3dab1facc5c
ruperthnyagesoa <ruperthnyagesoa@gmail.com>
2024-02-23 01:57:46 +0300

    ft:updates

---
+++ next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    swcMinify: true,
+    experimental: {
+    fontLoaders: [
+        {
+        loader: '@next/font/google',
+        },
+    ],
+    },
+}
+
+module.exports = nextConfig
